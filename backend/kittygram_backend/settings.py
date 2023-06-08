9103a9d6a1d7c541c42f891d0dddbbb3f89448e0
Pavel <room_pav@mail.ru>
2023-06-08 07:20:25 +0000

    Update settings, update env.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['158.160.66.199', '127.0.0.1', 'localhost', 'kittygramkitty.dynnamn.ru']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
